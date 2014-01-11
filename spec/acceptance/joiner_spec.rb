e373d7b7ca5b087e5540293f18d74416338e3f9d
Pat Allan <pat@freelancing-gods.com>
2014-01-11 15:11:07 +1100

    Working implementation via Thinking Sphinx.

--- spec/acceptance/joiner_spec.rb
+++ spec/acceptance/joiner_spec.rb
@@ -1,2 +1,84 @@
 require 'spec_helper'
 
+describe 'Joiner' do
+  it "handles has many associations" do
+    joiner = Joiner.new User
+    joiner.add_join_to [:articles]
+
+    sql = User.joins(joiner.join_values).to_sql
+    expect(sql).to match(/LEFT OUTER JOIN \"articles\"/)
+  end
+
+  it "handles multiple has many associations separately" do
+    joiner = Joiner.new User
+    joiner.add_join_to [:articles]
+    joiner.add_join_to [:articles, :comments]
+
+    sql = User.joins(joiner.join_values).to_sql
+    expect(sql).to match(/LEFT OUTER JOIN \"articles\"/)
+    expect(sql).to match(/LEFT OUTER JOIN \"comments\"/)
+  end
+
+  it "handles multiple has many associations together" do
+    joiner = Joiner.new User
+    joiner.add_join_to [:articles, :comments]
+
+    sql = User.joins(joiner.join_values).to_sql
+    expect(sql).to match(/LEFT OUTER JOIN \"articles\"/)
+    expect(sql).to match(/LEFT OUTER JOIN \"comments\"/)
+  end
+
+  it "handles a belongs to association" do
+    joiner = Joiner.new Comment
+    joiner.add_join_to [:article]
+
+    sql = Comment.joins(joiner.join_values).to_sql
+    expect(sql).to match(/LEFT OUTER JOIN \"articles\"/)
+  end
+
+  it "handles both belongs to and has many associations separately" do
+    joiner = Joiner.new Article
+    joiner.add_join_to [:user]
+    joiner.add_join_to [:comments]
+
+    sql = Article.joins(joiner.join_values).to_sql
+    expect(sql).to match(/LEFT OUTER JOIN \"users\"/)
+    expect(sql).to match(/LEFT OUTER JOIN \"comments\"/)
+  end
+
+  it "handles both belongs to and has many associations together" do
+    joiner = Joiner.new Article
+    joiner.add_join_to [:user, :comments]
+
+    sql = Article.joins(joiner.join_values).to_sql
+    expect(sql).to match(/LEFT OUTER JOIN \"users\"/)
+    expect(sql).to match(/LEFT OUTER JOIN \"comments\"/)
+  end
+
+  it "distinguishes joins via different relationships" do
+    joiner = Joiner.new Article
+    joiner.add_join_to [:comments]
+    joiner.add_join_to [:user, :comments]
+
+    expect(joiner.alias_for([:comments])).to eq('comments')
+    expect(joiner.alias_for([:user, :comments])).to eq('comments_users')
+  end
+
+  it "indicates aggregation for has many associations" do
+    joiner = Joiner.new User
+
+    expect(joiner.aggregate_for?([:articles])).to be_true
+  end
+
+  it "indicates non-aggregation for belongs to association" do
+    joiner = Joiner.new Article
+
+    expect(joiner.aggregate_for?([:user])).to be_false
+  end
+
+  it "determines the underlying model for an association path" do
+    joiner = Joiner.new User
+
+    expect(joiner.model_for([:articles, :comments])).to eq(Comment)
+  end
+end
